a1a4f24d7c23a5ff9b7e27bf57c1c39cc89786ee
wendell barcelos albano pestana <wendell_barcelos2@hotmail.com>
2022-10-13 18:28:57 -0300

    Criação de pacotes

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processing",
+    version="0.0.1",
+    author="Wendell",
+    author_email="my_email",
+    description="Image Processing Package using Skimage",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wendellbarcelos/BootcampDio-DataScienceUnimed/tree/master/Python%20para%20Data%20Science"
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
